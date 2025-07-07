9514fd92eadc136fe2ea24ffbfb624ee8158479e
SCC-Dev-256 <andrew@scctv.org>
2025-07-07 12:19:45 -0500

    Update Next.js build and server manifests, add new app pages and components for admin controls and visualization. Enhance layout and styling with new CSS and media files. Introduce detailed type definitions for app components.

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"nj7Md3wQZLECAXbxToYRVHbdNbc5XBiJ+X/axU0VnqI=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"vzID1CQp5X+I6o7qnOuKiZn5EzPR8VpiciSvbZGi6JE=\"\n}"
